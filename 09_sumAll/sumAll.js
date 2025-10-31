2053d716d0783b7ea58c5e1d75fe0bdea984f8be
Dev-Xanderr <snitramxander@gmail.com>
2025-10-31 14:15:51 +0400

    09-javaS Solved

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(min,max) {
+   
+    if (!Number.isInteger(min) || !Number.isInteger(max)){
+        return "ERROR";
+    }
+    if (min< 0 || max < 0){
+        return "ERROR";
+    }
+    if (min > max){
+        const tep = min;
+        min = max;
+        max = tep;
+    }
 
+
+    let totalSum = 0;
+    for(let i = min; i <= max ; i++){
+        totalSum += i
+    } 
+    return totalSum;
 };
-
 // Do not edit below this line
 module.exports = sumAll;
