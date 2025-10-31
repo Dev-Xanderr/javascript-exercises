2eba0d9acae8503ece551595a5032ef38b5ee376
Dev-Xanderr <snitramxander@gmail.com>
2025-10-31 13:46:37 +0400

    08-javaS Solved

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function (arr, ...rem) {
+    const newArray = [];
+    arr.forEach((item) => {
+        if (!rem.includes(item)){
+            newArray.push(item);
+        }
+    });
+    return newArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
